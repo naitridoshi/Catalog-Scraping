b27ed704559cb496b64e016b0dac8698b5705908
Naitri Doshi <jesaldoshi123@gmail.com>
2025-03-08 14:10:15 +0530

    Optimized Index values

--- common/db/__init__.py
+++ common/db/__init__.py
@@ -9,7 +9,9 @@ db= client[DATABASE_NAME]
 jinku_models_collection=db[JINKU_MODELS_COLLECTION_NAME]
 jinku_products_collection=db[JINKU_PRODUCTS_COLLECTION_NAME]
 
-jinku_products_collection.create_index('createdAt')
-jinku_products_collection.create_index('jinku_product_id')
+jinku_products_collection.create_index([("createdAt", -1)])
+jinku_products_collection.create_index([("jinku_product_id", 1)])
+jinku_products_collection.create_index([("jinku_product_id", -1)])
+
 
 
