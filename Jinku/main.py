331076c9574fd8a6957387692cb11c83cc139cf0
Naitri Doshi <jesaldoshi123@gmail.com>
2025-08-29 15:26:46 +0530

    Jinku update: Readme, main.py and request_helper.py

--- Jinku/main.py
+++ Jinku/main.py
@@ -1,29 +1,44 @@
 import os
 import time
+import pandas as pd
+import json
 
-from Jinku.constants import JINKU_PRODUCT_URL, JINKU_PRODUCT_URL_PARAMS
+from Jinku.constants import JINKU_PRODUCT_URL
 from common.custom_logger import get_logger
 from Jinku.request_helper import JinkuRequestHelper
 
 logger, listener = get_logger("Jinku Scraper")
 listener.start()
 
+def run_jinku_scraper(product_id: str) -> pd.DataFrame:
+    """
+    Runs the Jinku scraper for a given product ID.
+
+    Args:
+        product_id: The product ID to search for.
+
+    Returns:
+        A pandas DataFrame containing the scraped data, or an empty DataFrame if no data is found.
+    """
+    request_helper = JinkuRequestHelper()
+
+    logger.info(f"Getting Data for code - {product_id}")
+    main_url = f"{JINKU_PRODUCT_URL}?product_id={product_id}"
+
+    output_dir = "files/Jinku"
+    os.makedirs(output_dir, exist_ok=True)
+    output_filename = f"{output_dir}/jinku_products_{product_id}.json"
+
+    try:
+        df = request_helper.main(main_url, output_filename, return_df=True)
+        logger.info(f"Completed Scraping for code - {product_id} .... ")
+        return df
+
+    except Exception as e:
+        logger.error(f"An error occurred while scraping for product ID - {product_id} - {str(e)}")
+        return pd.DataFrame()
 
 
 if __name__ == '__main__':
-    request_helper = JinkuRequestHelper()
-
-    LETTER="40"  # ONLY CHANGE THIS. DO NOT CHANGE ANYTHING ELSE PLEASE
-
-    logger.info(f"Getting Data for code - {LETTER}")
-    main_url = f"{JINKU_PRODUCT_URL}?product_id={LETTER}"
-
-    request_helper.main(main_url,f"files/jinku_products_{LETTER}.json")
-
-    logger.info(f"Completed Scraping for code - {LETTER} .... ")
-
-    # for code in range(ord('a'), ord('a') + 1):
-    #     logger.info(f"Getting Data for code - {chr(code)}")
-    #     main_url=f"{JINKU_PRODUCT_URL}?product_id={chr(code)}"
-    #     request_helper.main(main_url,"files/jinku_products.json")
-    #     logger.info(f"Completed Scraping...")
+    LETTER = "40"  # ONLY CHANGE THIS. DO NOT CHANGE ANYTHING ELSE PLEASE
+    run_jinku_scraper(LETTER)
